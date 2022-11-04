da87ea5ec9e38c44c7f73258ed5834b22f60d915
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-11-04 00:00:42 -0500

    add find one by slug and create content types producto

--- src/api/producto/controllers/producto.js
+++ src/api/producto/controllers/producto.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * producto controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::producto.producto');
+module.exports = createCoreController(
+  "api::producto.producto",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
+
+      const entity = await strapi.db
+        .query("api::producto.producto")
+        .findOne({ 
+          where: { slug }, 
+          populate: {
+            IMAGEN: {
+              populate: {
+                fields: ['*'],
+              }
+            }
+          } 
+        });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
