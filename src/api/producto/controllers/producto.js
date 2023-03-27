09c7810090fa95d7bb15fefcb4db7f8eaff31329
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2023-03-27 00:40:45 -0500

    test method controller find by id for shopping cart

--- src/api/producto/controllers/producto.js
+++ src/api/producto/controllers/producto.js
@@ -30,3 +30,50 @@ module.exports = createCoreController(
     },
   })
 );
+
+// module.exports = createCoreController(
+//   "api::producto.producto",
+//   ({ strapi }) => ({
+//     async findOne(ctx) {
+//       const { slug } = ctx.params;
+
+//       const entity = await strapi.db
+//         .query("api::producto.producto")
+//         .findOne({ 
+//           where: { slug }, 
+//           populate: {
+//             IMAGEN: {
+//               populate: {
+//                 fields: ['*'],
+//               }
+//             }
+//           } 
+//         });
+//       const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+//       return this.transformResponse(sanitizedEntity);
+//     },
+    
+//     async findByIds(ctx) {
+//       const { ids } = ctx.query;
+
+//       const entities = await strapi.db
+//         .query("api::producto.producto")
+//         .find({
+//           where: {
+//             id_in: ids.split(",").map(Number)
+//           },
+//           populate: {
+//             IMAGEN: {
+//               populate: {
+//                 fields: ['*'],
+//               }
+//             }
+//           } 
+//         });
+//       const sanitizedEntities = await this.sanitizeOutput(entities, ctx);
+
+//       return this.transformResponse(sanitizedEntities);
+//     },
+//   })
+// );
